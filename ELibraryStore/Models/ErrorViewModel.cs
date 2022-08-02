5a7939084ed9fd2a148470f2249b4d4ba1b4656e
EMMANUEL <EMMANUEL@DESKTOP-FG6LILJ>
2022-08-02 16:38:40 +0100

    Add project files.

---
+++ ELibraryStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ELibraryStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
